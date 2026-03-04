c732aff2e2e5e7c868998b7eceab2eebf1421da1
Cruelasss <irula5198@gmail.com>
2026-03-04 09:16:58 +0700

    Initial commit

---
+++ src/main/java/com/deploy/pratikum1/Pratikum1Application.java
@@ -0,0 +1,13 @@
+package com.deploy.pratikum1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pratikum1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pratikum1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/deploy/pratikum1/Pratikum1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.pratikum1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pratikum1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
