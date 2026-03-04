1ef4f8881b61fa177df4578fee671874cc3df580
Cruelasss <irula5198@gmail.com>
2026-03-04 09:39:08 +0700

    menambahkan entity user

---
+++ src/main/java/com/deploy/pratikum1/model/entity/User.java
@@ -0,0 +1,27 @@
+package com.deploy.pratikum1.model.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table (name = "users")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @id
+    private String id;
+
+    @Column (name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "age",nullable = false)
+    private int age;
+}
