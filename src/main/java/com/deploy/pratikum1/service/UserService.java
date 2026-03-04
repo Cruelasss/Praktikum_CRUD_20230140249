5660968981a697c2b26d66cb9807f52cf83005c8
Cruelasss <irula5198@gmail.com>
2026-03-04 09:56:26 +0700

    feat: Menambahkan interface UserService untuk manajemen data User
    - Membuat interface UserService sebagai kontrak service untuk entitas User.
    - Menambahkan method AddUser() untuk menambah data user baru.
    - Menambahkan method getAllUser() untuk mengambil seluruh data user.
    - Menambahkan method getUserById() untuk mengambil user berdasarkan ID.
    - Menambahkan method UpdaetUser() untuk memperbarui data user berdasarkan ID.
    - Menambahkan method DeleteUser() untuk menghapus data user berdasarkan ID.

--- src/main/java/com/deploy/pratikum1/service/UserService.java
+++ src/main/java/com/deploy/pratikum1/service/UserService.java
@@ -1,4 +1,15 @@
 package com.deploy.pratikum1.service;
 
+import com.deploy.pratikum1.model.dto.UserAddRequest;
+import com.deploy.pratikum1.model.dto.UserDto;
+
+import java.util.List;
+
 public interface UserService {
+    UserDto AddUser (UserAddRequest request);
+    List<UserDto> getAllUser ();
+    UserDto getUserById(String id);
+    UserDto UpdaetUser (String id,  UserAddRequest request);
+    void DeleteUser(String id);
+
 }
